04607b3b458314cf1070ef3a89a9bf6341ee73f9
linlazy <linlazy@163.com>
2018-07-23 22:16:32 +0800

    other2

--- src/com/linlazy/Main.java
+++ src/com/linlazy/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    	int i=1;
-    	int i2=2;
-        String i3="1";
-        String i4="2";
+    		int []={1,2,3};
     }
 }
